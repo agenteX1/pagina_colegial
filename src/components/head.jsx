a941bba9132eb9f4d45549fd7a5d74a9a72fbf25
juandavidvalderramatoledo-gif <juandavidvalderramatoledo@gmail.com>
2026-06-01 15:10:15 -0600

    'barbas'

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,10 +1,21 @@
-export function Head (){
-
-    return(
-        <header className="header">
-            <div className="conten-logo">
-        <h1>perra</h1>
-        </div>
-        </header>
-    )
+export function Head() {
+  return (
+    <header className="header">
+      <div className="conten-logo">
+        <img
+          className="img-logo"
+          src="https://www.joseacevedo.edu.co/wp-content/uploads/2020/05/Escudo_IEJAG_png-1-915x1024.png"
+          alt="simbolo del colegio"
+        />
+        <h1>Institucion Educativa Jose Acevedo Gomez </h1>
+      </div>
+      <nav>
+        <a href="#inicio">inicio</a>
+        <a href="nuestro-coleguio">nuestro-coleguio</a>
+        <a href="horizonte institucional">horizonte institucional</a>
+        <a href="academia">academia</a>
+        <a href="proyectos">proyectos</a>
+      </nav>
+    </header>
+  );
 }
