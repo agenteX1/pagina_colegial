f036262f2acd1dba5e592f0db2911b6108d33be9
agenteX1 <mjhohanf.o@gmail.com>
2026-06-04 15:33:02 -0500

    hdah

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -11,23 +11,12 @@ export function Head() {
       </div>
       <nav className="nav">
         <a href="#inicio">inicio</a>
-<<<<<<< HEAD
         <a href="nuestro-colegio">nuestro colegio</a>
-=======
-        <a href=""></a>
-        <a href="nuestro-colegio">nuestro-colegio</a>
-        <a href=""></a>
->>>>>>> 01bce35fd3a2bd27d09701335c85f8e76eab5bc4
         <a href="horizonte institucional">horizonte institucional</a>
-        <a href=""></a>
         <a href="academia">academia</a>
-        <a href=""></a>
         <a href="proyectos">proyectos</a>
-        <a href=""></a>
         <a href="convivencia">convivencia</a>
-        <a href=""></a>
         <a href="izada de bandera">izada de bandera</a>
-        <a href=""></a>
         <a href="comunicados y temas de interes">
           comunicados y temas de interes
         </a>
