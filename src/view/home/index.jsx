795afa6e142b0ad97700a2ff7041c1e5fd91b408
arcilad542-pixel <arcilad542@gmail.com>
2026-07-09 15:55:06 -0600

    jijiji

--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -5,20 +5,7 @@ export function Home() {
   return (
     <div>
       <Hero />
-      <nav className="nav">
-        <a href="#inicio">inicio</a>
-        <a href="nuestro-colegio">nuestro colegio</a>
-        <a href="horizonte institucional">horizonte institucional</a>
-        <a href="academia">academia</a>
-        <a href="proyectos">proyectos</a>
-        <a href="convivencia">convivencia</a>
-        <a href="izada de bandera">izada de bandera</a>
-        <a href="comunicados y temas de interes">
-          comunicados y temas de interes
-        </a>
-        <a href="redes sociales">redes sociales</a>
-        <a href="recuerdos">recuerdos</a>
-      </nav>
+
       <Contens />
     </div>
   );
