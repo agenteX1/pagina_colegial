2bc5c967eca7d633d7b8091b9bea7307d16ce1df
agenteX1 <mjhohanf.o@gmail.com>
2026-07-16 16:02:26 -0500

    jaja

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,4 +1,13 @@
+import { Menu } from "lucide-react";
+import { useState } from "react";
+
 export function Head() {
+  const [viewmenu, setviewmenu] = useState(false);
+  /* FUNCION PARA OCULTA O DESPLEGAR EL MENU */
+  const Toogle = () => {
+    setviewmenu;
+  };
+
   return (
     <header className="header">
       <div className="conten-logo">
@@ -9,17 +18,22 @@ export function Head() {
         />
         <h1>I.E Jose Acevedo Gomez </h1>
       </div>
-      <nav className="nav">
-        <a href="#">inicio</a>
-        <a href="nuestro-colegio">nuestro colegio</a>
-        <a href="horizonte institucional">horizonte institucional</a>
-        <a href="academia">academia</a>
-        <a href="proyectos">proyectos</a>
-        <a href="convivencia">convivencia</a>
-        <a href="izada de bandera">izada de bandera</a>
-        <a href="comunicados y temas de interes">Noticias</a>
-        <a href="redes sociales">redes sociales</a>
-      </nav>
+      <button onClick={() => setviewmenu((prev) => !prev)}>
+        <Menu />
+      </button>
+      {viewmenu && (
+        <nav className="nav">
+          <a href="">inicio</a>
+          <a href="nuestro-colegio">nuestro colegio</a>
+          <a href="horizonte institucional">horizonte institucional</a>
+          <a href="academia">academia</a>
+          <a href="proyectos">proyectos</a>
+          <a href="convivencia">convivencia</a>
+          <a href="izada de bandera">izada de bandera</a>
+          <a href="comunicados y temas de interes">Noticias</a>
+          <a href="redes sociales">redes sociales</a>
+        </nav>
+      )}
     </header>
   );
 }
