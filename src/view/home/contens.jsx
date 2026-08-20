3e4430514ee12645702bf97110225a17ff5d79ae
agenteX1 <mjhohanf.o@gmail.com>
2026-08-20 14:51:49 -0500

    rrrrrrr

--- src/view/home/contens.jsx
+++ src/view/home/contens.jsx
@@ -1,10 +1,30 @@
 export function Contens() {
   return (
     <div className="contenedor1">
-      <h1 className="contents-title"></h1>
-      <div className="contenedor2">
-        <h1>raaaa</h1>
-      </div>
+      <div className="contenedor-2">
+    <h1>
+       Miura
+    </h1>
+        </div>
+        <div className="contenedor-2">
+     <img className="Countach" src="public/lamborghini Countach.jpg" alt="" />
+      <h1>
+         Countach
+     </h1>
+     <p>El Lamborghini Countach es el que redefinió la industria automotriz. Debutó 1971, perfil aerodinámico y puertas de tijera. Asienta las bases de los hiperautos modernos.</p>
+     <h3>$2.640.000 USD</h3>
+      <button className="btn2">comprar</button>
+        </div>
+         <div className="contenedor-2">
+     <img className="Revuelto" src="public/lamborghini Revuelto.jpg" alt="" />
+     <h1>
+         Revuelto
+    </h1>
+    <p>El Lamborghini Revuelto es el primer híbrido de la marca. Con una aceleración de 0 a 100 km/h en 2,5 segundos y una velocidad máxima de 350 km/h.
+    </p>
+    <h3>$604,363 USD</h3>
+    <button className="btn2">comprar</button>
+        </div>
     </div>
   );
 }
